9af1bded2426460a2f87330c750d817715c18d06
Ali Madooei <alimadooei@gmail.com>
2023-11-09 11:22:49 -0500

    Initial Commit

---
+++ src/user/user.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  // You can add more columns as required
+}
